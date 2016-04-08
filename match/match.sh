ff5a6286f4d82e49c7a5f87783e0932c43c91d9e
Alberto Real <albertorealfdez@gmail.com>
2016-04-08 10:07:21 +0200

    Changed documentation comment and little name refactor

--- match/match.sh
+++ match/match.sh
@@ -1,15 +1,17 @@
 # !/bin/bash
 
-# Match no common files between two directories taken by their hash
+# Match no common files between two directories taken by their hash.
+# It copies no common files in the first directory, compared to the second one.
+# Usage: match.sh folder-to-check folder-to-compare [destination-folder]
 #	- 1: first directory to match
 #	- 2: second directory to match
 #
 
 function getCommand() {
-	comm="md5sum"
+	command="md5sum"
 
 	if [ "$(uname)" = "Darwin" ]; then
-		comm="md5 -r"
+		command="md5 -r"
 	fi
 }
 
@@ -18,44 +20,44 @@ function main() {
 	dest="result/"
 
 	if [ $# -lt 2 ];  then
-		echo  "Usage: match.sh folder-to-check folder-to-compare destination-folder"
+		echo  "Usage: match.sh folder-to-check folder-to-compare [destination-folder]"
 		exit
 	elif 	[ "$#" = 3 ];  then
 		dest=$3
 	fi
 
-	getCommand comm
+	getCommand command
 
 	dir1=$1
 	dir2=$2
 	files1=$(find $dir1 -type f )
 	files2=$(find $dir2 -type f )
 	count=0
-	temp=0
+	times=0
+
+	mkdir -p $dest
 
 	if  [ "$(ls -A $dest)" ]; then
 		rm $dest/*	
-	else
-		mkdir -p $dest
 	fi
 
 	echo "Matching..."
 	for file1 in $files1; do
-		md5=$($comm $file1 | cut -d ' ' -f 1)
-		temp=0
+		md5=$($command $file1 | cut -d ' ' -f 1)
+		times=0
 		
 		for file2 in $files2; do		
-			md5n=$($comm $file2 | cut -d ' ' -f 1)		
+			md5n=$($command $file2 | cut -d ' ' -f 1)		
 			
 			if [ $md5 = $md5n ]; then
-				((temp++))
+				((times++))
 				break
 			fi
 		done
 		
-		if [ $temp = 0 ]; then
+		if [ $times = 0 ]; then
 			cp $file1 $dest
-			temp=0
+			times=0
 		fi
 		
 	done
